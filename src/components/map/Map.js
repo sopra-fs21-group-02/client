7bcb0cc652cbbe4051be3c64ce9f9a83ca5ce3f1
Benjamin Schneider <mail@benjaminschneider.ch>
2021-04-04 23:24:06 +0200

    Add tab navigation bar
    resolves #83

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import NavigationTab from '../../views/design/navigation/NavigationTab';
+import TabBar from '../../views/TabBar';
+import Users from '../../views/design/icons/Users';
 
 class Map extends React.Component {
   render() {
     return (
-      <h1>Show the map</h1>
+      <div>
+        <h1>Show the map</h1>
+
+        <div className="absolute inset-x-0 bottom-0">
+          <TabBar active="map" />
+        </div>
+      </div>
     );
   }
 }
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,12 +1,19 @@
 import React from "react"
 import { withRouter } from "react-router";
+import TabBar from "../../views/TabBar";
 
 class Profile extends React.Component {
-    render() {
-        return (
-            <h1>User's own profile!</h1>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>User's own profile!</h1>
+
+        <div className="absolute inset-x-0 bottom-0">
+          <TabBar active="profile" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Profile)
