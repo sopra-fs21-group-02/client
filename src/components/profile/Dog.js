84dd4b2971bca3ad463a73847a08454f58fb2168
Céline <celine@gmx.ch>
2021-04-19 17:25:20 +0200

    inputs updated
    #66

--- src/components/profile/Dog.js
+++ src/components/profile/Dog.js
@@ -127,7 +127,7 @@ class Dog extends React.Component {
                                 placeholder="Enter the name of your dog here (e.g. Fifi)."
                                 name = "name"
                                 value={this.state.dog.name}
-                                onChange={event => this.handleInputChange("name", event)}
+                                onChange={this.handleInputChange}
                                 className="w-full placeholder-grey rounded border h-9 p-2"
                             />
                         <h2 className="font-bold text-black mt-5">BREED</h2>
@@ -135,7 +135,7 @@ class Dog extends React.Component {
                             placeholder="Enter the breed of your dog here (e.g. Dalmatian)."
                             name = "breed"
                             value={this.state.dog.breed}
-                            onChange={event => this.handleInputChange("breed", event)}
+                            onChange={this.handleInputChange}
                             className="w-full placeholder-grey rounded border h-9 p-2"
                         />
                         <h2 className="font-bold text-black mt-5">DATE OF BIRTH</h2>
@@ -143,7 +143,7 @@ class Dog extends React.Component {
                             placeholder="Enter the date of birth of your dog here (e.g. 2018-04-01)."
                             name = "dateOfBirth"
                             value={this.state.dog.dateOfBirth}
-                            onChange={event => this.handleInputChange("dateOfBirth", event)}
+                            onChange={this.handleInputChange}
                             className="w-full placeholder-grey rounded border h-9 p-2"
                         />
 
@@ -164,9 +164,9 @@ class Dog extends React.Component {
                     <div className="bg-cover bg-no-repeat bg-center h-20 w-20 rounded-full" style={{
                         backgroundImage: `url(${this.state.dog.imageUrl})`,
                     }}>
-                        <div className="group-hover:opacity-100">
+                        <div className="group-hover:opacity-100 group-hover: cursor-pointer">
                             <div className="hover:bg-gray-300 cursor-pointer h-20 w-20 rounded-full flex flex-col opacity-80">
-                                <div className="m-auto">
+                                <div className="m-auto mt-6">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M17.7376 1.7376C18.0328 1.43197 18.3859 1.18819 18.7763 1.02048C19.1667 0.85277 19.5866 0.764494 20.0115 0.760802C20.4364 0.75711 20.8578 0.838075 21.2511 0.998974C21.6443 1.15987 22.0016 1.39748 22.3021 1.69794C22.6025 1.9984 22.8401 2.35568 23.001 2.74895C23.1619 3.14222 23.2429 3.56359 23.2392 3.98849C23.2355 4.41338 23.1472 4.83329 22.9795 5.2237C22.8118 5.61411 22.568 5.96721 22.2624 6.2624L20.9936 7.5312L16.4688 3.0064L17.7376 1.7376ZM14.2064 5.2688L0.8 18.6752V23.2H5.3248L18.7328 9.7936L14.2048 5.2688H14.2064Z" fill="black"/>
                                     </svg>
@@ -174,8 +174,8 @@ class Dog extends React.Component {
                                 <input type="file"
                                        accept="image/*"
                                        name="imageUrl"
-                                       onChange={event => this.handleInputChange("imageUrl", event)}
-                                       className="upload mb-2 mt-0 text-grey"
+                                       onChange={this.handleInputChange}
+                                       className="upload bg-cover bg-no-repeat bg-center h-20 w-20 rounded-full"
                                 />
                             </div>
                         </div>
@@ -206,7 +206,7 @@ class Dog extends React.Component {
 
     //TODO adapt method once API is integrated
     saveDog(){
-        if (!this.state.sex || !this.state.name || !this.state.breed || !this.state.dateOfBirth){
+        if (!this.state.dog.sex || !this.state.dog.name || !this.state.dog.breed || !this.state.dog.dateOfBirth){
             alert("please enter all attributes of your dog. Name, breed, date of birth and sex")
         }
         else {this.redirectToProfile()}
@@ -219,24 +219,26 @@ class Dog extends React.Component {
     };
 
     //TODO adapt method once API is integrated
-    handleInputChange(key, value) {
-        this.setState({dog :{ [key]: value }});
-    }
-    /*
     handleInputChange(event) {
-        const target = event.target;
-        const value = event.value;
-        const name = target.name;
-
-        this.setState({ dog :{
-            [name]: value}
-        });
-     */
+        this.setState(prevState => {
+            let dog = Object.assign({}, prevState.dog);
+            dog[event.target.name] = event.target.value;
+            //dog.event.target.name = event.target.value
+            console.log(this.state.dog)
+            return { dog };
+        })
+    }
 
     //TODO adapt method once API is integrated
     saveSex(sex) {
-        this.setState({dog : {sex : sex}})
+        this.setState(prevState => {
+            let dog = Object.assign({}, prevState.dog);
+            dog.sex = sex;
+            console.log(this.state.dog)
+            return { dog };
+        })
         this.setState({active : sex})
+        console.log(this.state.dog)
     }
 }
 
