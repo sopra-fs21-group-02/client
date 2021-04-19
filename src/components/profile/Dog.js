140c2911733f55850bec90b648346a0fa10687ea
Céline <celine@gmx.ch>
2021-04-19 13:22:16 +0200

    changes on input
    #66

--- src/components/profile/Dog.js
+++ src/components/profile/Dog.js
@@ -2,21 +2,6 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import styled from "styled-components";
 
-const InputField = styled.input`
-  &::placeholder {
-    color: gray;
-    height: 35px;
-  }
-  height: 35px;
-  padding-left: 8px;
-  margin-right: 0px;
-  margin-left: 3px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-bottom: 20px;
-  background: rgba(255, 255, 255, 0.2);
-  color: black;
-`;
 
 const ALL_DOGS = [
     {
@@ -58,6 +43,11 @@ const FAKE_FETCH_DOGS = (id) => {
 class Dog extends React.Component {
     constructor() {
         super();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.saveSex = this.saveSex.bind(this);
+        this.saveDog = this.saveDog.bind(this);
+        this.redirectToAddImage = this.redirectToAddImage.bind(this);
+        this.redirectToProfile = this.redirectToProfile.bind(this);
         this.state = {
             active : false,
             dog :{
@@ -80,7 +70,7 @@ class Dog extends React.Component {
                     breed: "",
                     sex: "",
                     name: "",
-                    imageUrl: "https://www.pdsa.org.uk/media/7888/dalmatian-gallery-outdoors-8-min.jpg",
+                    imageUrl: "https://thumbs.dreamstime.com/z/puppy-vector-illustration-isolated-white-background-dog-line-art-puppy-vector-illustration-cute-cartoon-dog-140539394.jpg",
                     dateOfBirth: "",
                     id: this.props.match.params.dogId
                 }
@@ -88,7 +78,8 @@ class Dog extends React.Component {
         } else { // Editing dog
             let dogServer = FAKE_FETCH_DOGS(parseInt(routeId));
             await this.setState({
-                dog: dogServer
+                dog: dogServer,
+                active : dogServer.sex
             });
         }
     }
@@ -96,20 +87,20 @@ class Dog extends React.Component {
     render() {
         let container = "cursor-pointer hover:bg-gray-400 flex-grow inline-block p-3 mr-2 bg-gray-300 text-xs rounded-md";
         let containerMale=container;
-        if(this.state.active === "male") {
+        if(this.state.active === "MALE") {
             containerMale += " bg-gray-400";
         } else {
             containerMale += " bg-gray-300";
         }
         let containerFemale=container;
-        if(this.state.active === "female") {
+        if(this.state.active === "FEMALE") {
             containerFemale += " bg-gray-400";
         } else {
             containerFemale += " bg-gray-300";
         }
 
         let saveContainer = "";
-        if (!this.state.sex || !this.state.name || !this.state.breed || !this.state.dateOfBirth) {
+        if (!this.state.dog.sex || !this.state.dog.name || !this.state.dog.breed || !this.state.dog.dateOfBirth) {
             saveContainer += "h-12 font-bold text-xl align-middle pt-2.5 w-1/2 opacity-20 cursor-not-allowed"
         }
         else {
@@ -120,7 +111,10 @@ class Dog extends React.Component {
             <div className="h-screen">
                 <div className="">
                     <div className=" h-12 bg-gray-300 text-center">
-                        <h1 className="font-bold text-xl align-middle pt-2.5">New Dog</h1>
+                        {(!this.state.dog.sex || !this.state.dog.name || !this.state.dog.breed || !this.state.dog.dateOfBirth) ?
+                            <h1 className="font-bold text-xl align-middle pt-2.5">New Dog</h1> :
+                            <h1 className="font-bold text-xl align-middle pt-2.5">Edit Dog</h1>
+                        }
                     </div>
                 </div>
 
@@ -129,40 +123,38 @@ class Dog extends React.Component {
                         <h2 className="font-bold text-black">NAME</h2>
 
                         {/*TODO if value – it should be changeable*/}
-                        <InputField
-                            placeholder="Fifi"
-                            value = {this.state.dog.name}
-                            onChange={e => {
-                                this.handleInputChange('name', e.target.value);
-                            }}
-                            className="w-full"
+                            <input
+                                placeholder="Enter the name of your dog here (e.g. Fifi)."
+                                name = "name"
+                                value={this.state.dog.name}
+                                onChange={event => this.handleInputChange("name", event)}
+                                className="w-full placeholder-grey rounded border h-9 p-2"
+                            />
+                        <h2 className="font-bold text-black mt-5">BREED</h2>
+                        <input
+                            placeholder="Enter the breed of your dog here (e.g. Dalmatian)."
+                            name = "breed"
+                            value={this.state.dog.breed}
+                            onChange={event => this.handleInputChange("breed", event)}
+                            className="w-full placeholder-grey rounded border h-9 p-2"
                         />
-                        <h2 className="font-bold text-black">BREED</h2>
-
-                        <InputField
-                            placeholder="Dalmatian"
-                            onChange={e => {
-                                this.handleInputChange('breed', e.target.value);
-                            }}
-                            className="w-full"
-                        />
-                        <h2 className="font-bold text-black">DATE OF BIRTH</h2>
-                        <InputField
-                            placeholder="1.1.2021"
-                            onChange={e => {
-                                this.handleInputChange('dateOfBirth', e.target.value);
-                            }}
-                            className="w-full"
+                        <h2 className="font-bold text-black mt-5">DATE OF BIRTH</h2>
+                        <input
+                            placeholder="Enter the date of birth of your dog here (e.g. 2018-04-01)."
+                            name = "dateOfBirth"
+                            value={this.state.dog.dateOfBirth}
+                            onChange={event => this.handleInputChange("dateOfBirth", event)}
+                            className="w-full placeholder-grey rounded border h-9 p-2"
                         />
 
                         {/* select the sex*/}
-                        <h2 className="font-bold text-black">SEX</h2>
+                        <h2 className="font-bold text-black mt-5">SEX</h2>
                         <div className={containerFemale}
-                                onClick={() => this.saveSex("female")}>
+                                onClick={() => this.saveSex("FEMALE")}>
                             <h3 className="font-bold leading-none">Female</h3>
                         </div>
                         <div className={containerMale}
-                                onClick={() => this.saveSex("male")}>
+                                onClick={() => this.saveSex("MALE")}>
                             <h3 className="font-bold leading-none">Male</h3>
                         </div>
                     </div>
@@ -170,16 +162,21 @@ class Dog extends React.Component {
                     {/* adding image here*/}
                     <div className="flex mr-2 mt-2">
                     <div className="bg-cover bg-no-repeat bg-center h-20 w-20 rounded-full" style={{
-                        backgroundImage: `url(${this.state.imageUrl})`,
+                        backgroundImage: `url(${this.state.dog.imageUrl})`,
                     }}>
                         <div className="group-hover:opacity-100">
-                            <div className="hover:bg-gray-300 cursor-pointer h-20 w-20 rounded-full flex flex-col opacity-80"
-                                    onClick={() => this.redirectToAddImage()}>
+                            <div className="hover:bg-gray-300 cursor-pointer h-20 w-20 rounded-full flex flex-col opacity-80">
                                 <div className="m-auto">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M17.7376 1.7376C18.0328 1.43197 18.3859 1.18819 18.7763 1.02048C19.1667 0.85277 19.5866 0.764494 20.0115 0.760802C20.4364 0.75711 20.8578 0.838075 21.2511 0.998974C21.6443 1.15987 22.0016 1.39748 22.3021 1.69794C22.6025 1.9984 22.8401 2.35568 23.001 2.74895C23.1619 3.14222 23.2429 3.56359 23.2392 3.98849C23.2355 4.41338 23.1472 4.83329 22.9795 5.2237C22.8118 5.61411 22.568 5.96721 22.2624 6.2624L20.9936 7.5312L16.4688 3.0064L17.7376 1.7376ZM14.2064 5.2688L0.8 18.6752V23.2H5.3248L18.7328 9.7936L14.2048 5.2688H14.2064Z" fill="black"/>
                                     </svg>
                                 </div>
+                                <input type="file"
+                                       accept="image/*"
+                                       name="imageUrl"
+                                       onChange={event => this.handleInputChange("imageUrl", event)}
+                                       className="upload mb-2 mt-0 text-grey"
+                                />
                             </div>
                         </div>
                     </div>
@@ -218,16 +215,27 @@ class Dog extends React.Component {
     redirectToAddImage() {
         let url = "/profile/dog/new/image";
         this.props.history.push(url);
+
     };
 
     //TODO adapt method once API is integrated
     handleInputChange(key, value) {
-        this.setState({ [key]: value });
+        this.setState({dog :{ [key]: value }});
     }
+    /*
+    handleInputChange(event) {
+        const target = event.target;
+        const value = event.value;
+        const name = target.name;
+
+        this.setState({ dog :{
+            [name]: value}
+        });
+     */
 
     //TODO adapt method once API is integrated
     saveSex(sex) {
-        this.setState({sex : sex})
+        this.setState({dog : {sex : sex}})
         this.setState({active : sex})
     }
 }
