366f56a6ee3544bf6e1fe35f93d7a889f52a2d2f
Benjamin Schneider <mail@benjaminschneider.ch>
2021-04-22 13:39:20 +0200

    Fix indentation/formatting

--- src/components/profile/EditDog.js
+++ src/components/profile/EditDog.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class EditDog extends React.Component {
-    render() {
-        return (
-            <h1>Editing page of Dog with ID {this.props.match.params.dogId}</h1>
+  render() {
+    return (
+      <h1>Editing page of Dog with ID {this.props.match.params.dogId}</h1>
     );
-    }
+  }
 }
 
 export default withRouter(EditDog);
--- src/components/profile/NewDog.js
+++ src/components/profile/NewDog.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class NewDog extends React.Component {
-    render() {
-        return (
-            <h1>Adding new Dog</h1>
-        );
-    }
+  render() {
+    return (
+      <h1>Adding new Dog</h1>
+    );
+  }
 }
 
 export default withRouter(NewDog);
