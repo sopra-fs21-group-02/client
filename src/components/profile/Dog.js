973927bd8dfa88878508b9906df05a80e88172ac
Céline <celine@gmx.ch>
2021-04-22 12:25:08 +0200

    changes on image upload
    #66 #69

--- src/components/profile/Dog.js
+++ src/components/profile/Dog.js
@@ -41,13 +41,15 @@ const FAKE_FETCH_DOGS = (id) => {
 }
 
 class Dog extends React.Component {
+
     constructor() {
         super();
         this.handleInputChange = this.handleInputChange.bind(this);
         this.saveSex = this.saveSex.bind(this);
         this.saveDog = this.saveDog.bind(this);
-        this.redirectToAddImage = this.redirectToAddImage.bind(this);
+        this.saveImage = this.saveImage.bind(this);
         this.redirectToProfile = this.redirectToProfile.bind(this);
+        this.inputRef = React.createRef();
         this.state = {
             active : false,
             dog :{
@@ -62,6 +64,7 @@ class Dog extends React.Component {
     }
 
     async componentDidMount() {
+        this.inputRef.current.focus();
         let routeId = this.props.match.params.dogId;
         console.log(routeId)
         if (routeId === "new") { // New dog
@@ -121,8 +124,6 @@ class Dog extends React.Component {
                 <div className="pt-2 pr-2 flex flex-cols">
                     <div className="flex-grow ml-5 mt-2 pr-10">
                         <h2 className="font-bold text-black">NAME</h2>
-
-                        {/*TODO if value – it should be changeable*/}
                             <input
                                 placeholder="Enter the name of your dog here (e.g. Fifi)."
                                 name = "name"
@@ -166,17 +167,23 @@ class Dog extends React.Component {
                     }}>
                         <div className="group-hover:opacity-100 group-hover: cursor-pointer">
                             <div className="hover:bg-gray-300 cursor-pointer h-20 w-20 rounded-full flex flex-col opacity-80">
-                                <div className="m-auto mt-6">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M17.7376 1.7376C18.0328 1.43197 18.3859 1.18819 18.7763 1.02048C19.1667 0.85277 19.5866 0.764494 20.0115 0.760802C20.4364 0.75711 20.8578 0.838075 21.2511 0.998974C21.6443 1.15987 22.0016 1.39748 22.3021 1.69794C22.6025 1.9984 22.8401 2.35568 23.001 2.74895C23.1619 3.14222 23.2429 3.56359 23.2392 3.98849C23.2355 4.41338 23.1472 4.83329 22.9795 5.2237C22.8118 5.61411 22.568 5.96721 22.2624 6.2624L20.9936 7.5312L16.4688 3.0064L17.7376 1.7376ZM14.2064 5.2688L0.8 18.6752V23.2H5.3248L18.7328 9.7936L14.2048 5.2688H14.2064Z" fill="black"/>
-                                    </svg>
+                                <div>
+                                    <label htmlFor="fileUpload">
+                                        <div>
+                                            <div className="m-7">
+                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M17.7376 1.7376C18.0328 1.43197 18.3859 1.18819 18.7763 1.02048C19.1667 0.85277 19.5866 0.764494 20.0115 0.760802C20.4364 0.75711 20.8578 0.838075 21.2511 0.998974C21.6443 1.15987 22.0016 1.39748 22.3021 1.69794C22.6025 1.9984 22.8401 2.35568 23.001 2.74895C23.1619 3.14222 23.2429 3.56359 23.2392 3.98849C23.2355 4.41338 23.1472 4.83329 22.9795 5.2237C22.8118 5.61411 22.568 5.96721 22.2624 6.2624L20.9936 7.5312L16.4688 3.0064L17.7376 1.7376ZM14.2064 5.2688L0.8 18.6752V23.2H5.3248L18.7328 9.7936L14.2048 5.2688H14.2064Z" fill="black"/>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </label>
+                                    <input hidden id="fileUpload"
+                                           type="file"
+                                           accept="image/*"
+                                           ref={this.inputRef}
+                                           onChange={this.showOpenFileDlg}
+                                           className="upload bg-cover bg-no-repeat bg-center h-20 w-20 rounded-full"/>
                                 </div>
-                                <input type="file"
-                                       accept="image/*"
-                                       name="imageUrl"
-                                       onChange={this.handleInputChange}
-                                       className="upload bg-cover bg-no-repeat bg-center h-20 w-20 rounded-full"
-                                />
                             </div>
                         </div>
                     </div>
@@ -199,6 +206,11 @@ class Dog extends React.Component {
         );
     }
 
+    showOpenFileDlg = () => {
+        this.inputRef.current.click()
+        this.saveImage(this.inputRef)
+    }
+
     //TODO adapt method once API is integrated
     redirectToProfile(){
         this.props.history.push("/profile");
@@ -211,12 +223,6 @@ class Dog extends React.Component {
         }
         else {this.redirectToProfile()}
     }
-
-    redirectToAddImage() {
-        let url = "/profile/dog/new/image";
-        this.props.history.push(url);
-
-    };
 
     //TODO adapt method once API is integrated
     handleInputChange(event) {
@@ -238,6 +244,16 @@ class Dog extends React.Component {
             return { dog };
         })
         this.setState({active : sex})
+        console.log(this.state.dog)
+    }
+
+    saveImage(image) {
+        this.setState(prevState => {
+            let dog = Object.assign({}, prevState.dog);
+            dog.imageUrl = image;
+            console.log(this.state.dog)
+            return { dog };
+        })
         console.log(this.state.dog)
     }
 }
