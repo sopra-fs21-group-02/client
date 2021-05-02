79a72ca2ad632c0bd8c2dd05fcab07fbff688616
Benjamin Schneider <mail@benjaminschneider.ch>
2021-05-02 15:12:28 +0200

    Add remarks on login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -27,26 +27,26 @@ class Login extends React.Component {
     };
   }
 
-  async onLogin(response){
-    const requestBody =JSON.stringify({
+  async onLogin(response) {
+    const requestBody = JSON.stringify({
       tokenId: response.tokenId,
       emailId: response.profileObj.email
     });
 
-    try{
+    try {
       const res = await api.post("v1/users/login", requestBody);
 
       localStorage.setItem('accessToken', res.data.accessToken);
       localStorage.setItem('accessTokenExpiry', res.data.accessTokenExpiry);
 
-      if(res.data.isNewUser){
+      if (res.data.isNewUser) {
         this.props.history.push('/profile');
-      }else{
+      } else {
         this.props.history.push('/map');
       }
-    }catch (error) {
+    } catch (error) {
       const errMessage = handleError(error);
-      this.setState({errorMessage:errMessage});
+      this.setState({ errorMessage: errMessage });
     };
   }
 
@@ -56,28 +56,33 @@ class Login extends React.Component {
       this.onLogin(response);
     }
     return (
-        <div className="flex flex-col h-screen w-full">
+      <div className="flex flex-col h-screen w-full">
 
-          <div className="flex-1 mt-10 z-50">
-            <div>
-              <h1 className="font-bold text-4xl text-center mt-10">🐕 </h1>
-              <h2 className="font-bold text-2xl text-center mt-3">Friendly Fetch</h2>
-              <h3 className=" text-l text-center mt-3">Welcome! Please sign in below to use the app.</h3>
-            </div>
-            <div className="flex-1 ml-auto mr-auto mt-10">
-              <div className="flex-col items-center flex justify-center">
-                <GoogleLogin
-                  clientId={getClientId()}
-                  buttonText="Sign in with Google"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
-                  cookiePolicy={'single_host_origin'}
-                />
-                <ErrorMessage>{this.state.errorMessage}</ErrorMessage>
-              </div>
+        <div className="flex-1 mt-10 z-50">
+          <div>
+            <h1 className="font-bold text-4xl text-center mt-10">🐕 </h1>
+            <h2 className="font-bold text-2xl text-center mt-3">Friendly Fetch</h2>
+            <h3 className=" text-l text-center mt-3">Welcome! Please sign in below to use the app.</h3>
+          </div>
+          <div className="flex-1 ml-auto mr-auto mt-10">
+            <div className="flex-col items-center flex justify-center">
+              <GoogleLogin
+                clientId={getClientId()}
+                buttonText="Sign in with Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+              />
+              <ErrorMessage>{this.state.errorMessage}</ErrorMessage>
             </div>
           </div>
+          <div>
+            <p className="text-sm mt-8 text-gray-500 w-2/3 mx-auto">
+              Please note that this application is optimized for smartphone screen sizes and requires location permissions as well as third-party cookies to be enabled.
+              </p>
+          </div>
         </div>
+      </div>
 
     );
   }
